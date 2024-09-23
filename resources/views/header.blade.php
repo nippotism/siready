769f56e49d9cf7f3c3af672c0fe00028fb6cded9
nippotism <syarifulhanif@gmail.com>
2024-09-23 16:23:31 +0700

    favicon

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&family=Poppins:wght@200;400&display=swap"
     rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="siready2.png">
     <title>@yield('title') - Siready</title>
 
     {{-- jquery --}}
@@ -18,7 +19,7 @@
     {{-- datatables --}}
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.6/css/dataTables.dataTables.css" />
     <script src="https://cdn.datatables.net/2.1.6/js/dataTables.js"></script>
-    
+
 
 </head>
 
