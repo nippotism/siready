f85e07de046331d4c4aa90394c337f0ca7a0c507
Yudhis Ahsan <yudhisahsan@gmail.com>
2024-10-31 19:36:45 +0700

    create api registration

---
+++ app/Http/Controllers/Api/Register.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Mahasiswa;
+use App\Models\User;
+
+class Register extends Controller
+{
+    //
+
+    public function registerStatus(Request $request)
+    {
+
+        // return response()->json(['message' => 'Status Mahasiswa Berhasil Diubah', 'data' => $request->all()]);
+
+        
+        //change status mahasiwa where email  = email request body
+        $email  = $request->email;
+        $status = $request->status;
+        $mahasiswa = User::where('email', $email)->first();
+        $mahasiswa->status = $status;
+        // return response()->json(['message' => 'Status Mahasiswa Berhasil Diubah', 'data' => $mahasiswa]);
+        
+        $mahasiswa->save();
+
+        $data = Mahasiswa::where('email', $email)->first();
+
+        return response()->json(['message' => 'Status Mahasiswa Berhasil Diubah', 'data' => $mahasiswa]);
+
+        
+        
+    }
+
+    public function statusMahasiswa()
+    {
+        $data = Mahasiswa::all();
+        return response()->json(['data' => $data]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Register;
 
+
+Route::post('/register', [Register::class, 'registerStatus']);
+Route::get('/register', [Register::class, 'statusMahasiswa']);
